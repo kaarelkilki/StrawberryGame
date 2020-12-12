cba7ea782779cac78f004bfbb36017703390d891
Kaarel Kilki <kaarel.kilki@gmail.com>
2020-12-12 11:02:00 +0200

    Buttons

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 15;
     private Vector3 moveDir;
+    public Button left;
+    public Button rigth;
     void Update()
     {
-        moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        moveDir = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical")).normalized;
     }
     void FixedUpdate()
     {
