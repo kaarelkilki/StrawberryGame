0850adb692af277e9680df03719c28969319f616
Kaarel Kilki <kaarel.kilki@gmail.com>
2020-12-12 05:47:35 +0200

    FauxGravity, ursus etc.

---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAtractor attractor;
+    private Transform myTransform;
+    void Start()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 15;
+    private Vector3 moveDir;
+    void Update()
+    {
+        moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+    }
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+    }
+}
