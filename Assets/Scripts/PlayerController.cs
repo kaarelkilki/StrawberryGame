2171af5fc4fb396864ce011eeb06821e7fdc5b50
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2020-12-17 05:43:46 +0200

    Automatic movement + ligthing fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float thrust;
-    //public float forwardForce = 15;
     public float moveSpeed = 15;
     private Vector3 moveDir;
+
     public Button left;
     public Button right;
 
     private int count;
 
     public Text countText;
+
     void Start()
     {
         count = 0;
+        
+        
     }
     void Update()
     {
@@ -27,18 +28,7 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        //rb.AddRelativeForce(Vector3.forward * thrust);
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
-        
-        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        //if(Input.GetKey("d"))
-        //{
-        //    rb.AddForce(500 * Time.deltaTime, 0, 0);
-        //}
-        //if (Input.GetKey("a"))
-        //{
-        //    rb.AddForce(-500 * Time.deltaTime, 0, 0);
-        //}
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + this.transform.forward * moveSpeed * Time.deltaTime);
     }
     private void OnTriggerEnter(Collider other)
     {
