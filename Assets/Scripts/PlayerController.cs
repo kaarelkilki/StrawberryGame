ff09973047aa689fcea765d7508196477eb99bbe
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2020-12-19 07:32:06 +0200

    LevelText, LoadNextLevel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,23 +12,27 @@ public class PlayerController : MonoBehaviour
     public Button left;
     public Button right;
     public TMP_Text scoreText;
+    public TMP_Text levelText;
 
     private Vector3 moveDir;
     private Rigidbody rb;
     private int count;
-    
+
+    Scene scene;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetScoreText();
+        SetLevelText();
     }
     void Update()
     {
         //moves player left and right
         moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, 0).normalized;
 
+        //player movement in Android device
         //if (Input.touchCount > 0)
         //{
         //    Touch touch = Input.GetTouch(0);
@@ -40,6 +45,8 @@ public class PlayerController : MonoBehaviour
         //        GoLeft();
         //    }
         //}
+
+        LoadNextLevel();
     }
     void FixedUpdate()
     {
@@ -61,5 +68,17 @@ public class PlayerController : MonoBehaviour
     void SetScoreText()
     {
         scoreText.text = count.ToString() + " <sprite=0>";
+    }
+    void SetLevelText()
+    {
+        scene = SceneManager.GetActiveScene();
+        levelText.text = "Level " + scene.buildIndex;
+    }
+    void LoadNextLevel()
+    {
+        if (count == 15)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
