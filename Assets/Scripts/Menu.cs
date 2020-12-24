1a2825a4e0d527ff1ed1e3b13041f43415fa5bb7
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2020-12-24 07:25:57 +0200

    imageLeft and imageRight

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,18 +7,6 @@ using TMPro;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ExitButton()
     {
         Application.Quit();
