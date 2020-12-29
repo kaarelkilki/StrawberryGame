32d3a862a2b75e9a340ce22b73ddb91ab0c3d1bc
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2020-12-29 06:32:41 +0200

    RewardedAdsPlaceent

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsListener
+{
+    string placement = "rewardedVideo";
+    
+    void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize("3953951", true);
+    }
+
+    public void ShowAd(string p)
+    {
+        Advertisement.Show(p);
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Finished)
+        {
+            //Give 15 extra seconds to use
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("The ad did not finish due to an error.");
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+
+    }
+}
